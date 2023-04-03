d020bf50c78184c21f1b04f7b5384d4bf57a8afd
shiravand <shiravandm96@gmail.com>
2023-04-03 13:37:10 +0430

    Refactor IEntitiyMapper

--- src/main/java/dev/shiravand/mongodbdemo/shared/IEntityMapper.java
+++ src/main/java/dev/shiravand/mongodbdemo/shared/IEntityMapper.java
@@ -1,7 +1,7 @@
 package dev.shiravand.mongodbdemo.shared;
 
-public interface IEntityMapper<M, D> {
-    public M toEntity(D dto);
-    public D toDto(M entity);
+public interface IEntityMapper<ENT, DTO> {
+    public ENT toEntity(DTO dto);
+    public DTO toDto(ENT entity);
 
 }
